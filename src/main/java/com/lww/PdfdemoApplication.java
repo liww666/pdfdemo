26f79fea8f6739b9f1ec175b102db18b061a17da
liweiwu <liweiwu9205@dingtalk.com>
2024-09-02 22:26:11 +0800

    first commit

---
+++ src/main/java/com/lww/PdfdemoApplication.java
@@ -0,0 +1,11 @@
+package com.lww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfdemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PdfdemoApplication.class, args);
+    }
+}
